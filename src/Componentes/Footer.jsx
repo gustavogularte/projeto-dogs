f4e0297dd2c1aa66bfa084d05e66479c9db7214a
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2023-12-01 23:50:22 -0300

    footer

--- src/Componentes/Footer.jsx
+++ src/Componentes/Footer.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import styles from './Footer.module.css'
+import styles from './Footer.module.css';
+import Dogs from '../Assets/dogs-footer.svg'
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <footer>
-      Footer
+    <footer className={styles.footer}>
+      <Link to='/'><img src={Dogs} alt="Logo Dogs" /></Link>
+      <p><a href="https://www.origamid.com" target="_blank" rel='noreferrer'>Origamid</a> © Projeto feito por <a href="https://github.com/gustavogularte" target="_blank" rel='noreferrer'>Gustavo Gularte Arend</a>.</p>
     </footer>
   )
 }
