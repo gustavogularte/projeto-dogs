5466dd749473f2b4309f906df995cab3c0ccba57
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2023-12-05 20:02:22 -0300

    postar estilos

--- src/Componentes/User/UserPostar.jsx
+++ src/Componentes/User/UserPostar.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Input from '../Forms/Input'
+import useForm from '../../Hooks/useForm';
+import Button from '../Forms/Button';
 
 const UserPostar = () => {
+  const nome = useForm();
+  const peso = useForm();
+  const idaide = useForm();
+
   return (
-    <div>
-      Postar
-    </div>
+    <form>
+      <Input label='Nome' type='text' />
+      <Input label='Peso' type='text' />
+      <Input label='Idade' type='text' />
+      <input type='file' />
+      <Button>Enviar</Button>
+    </form>
   )
 }
 
