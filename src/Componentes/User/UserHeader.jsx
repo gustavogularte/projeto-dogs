6316d632d86a6e1564b6ea230e38c1b02582efbf
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2023-12-05 19:55:59 -0300

    titulos

--- src/Componentes/User/UserHeader.jsx
+++ src/Componentes/User/UserHeader.jsx
@@ -1,22 +1,53 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import Conta from '../../Assets/feed.svg?react';
 import Estatisticas from '../../Assets/estatisticas.svg?react';
 import Postar from '../../Assets/adicionar.svg?react';
+import Sair from '../../Assets/sair.svg?react'
 import styles from './UserHeader.module.css';
 import { UserContext } from '../../UserContext';
 
 const UserHeader = () => {
-  const {logoutUser} = React.useContext(UserContext);
+  const { logoutUser } = React.useContext(UserContext);
+  const [titulo, setTitulo] = React.useState('');
+  const location = useLocation().pathname;
+
+  React.useEffect(() => {
+    const mudarTitulo = () => {
+      switch (location) {
+        case '/conta':
+          setTitulo('Minha conta');
+          break;
+        case '/conta/estatisticas':
+          setTitulo('Estatísticas');
+          break;
+        case '/conta/postar':
+          setTitulo('Poste sua foto');
+      }
+    };
+    mudarTitulo();
+  }, [location]);
 
   return (
     <section className={styles.header}>
-      <h1 className="titulo">Minha conta</h1>
+      <h1 className="titulo">{titulo}</h1>
       <nav className={styles.nav}>
-        <li><NavLink to='/conta'><Conta/></NavLink></li>
-        <li><NavLink to='/conta/estatisticas'><Estatisticas/></NavLink></li>
-        <li><NavLink to='/conta/postar'><Postar/></NavLink></li>
-        <button onClick={logoutUser}>Sair</button>
+        <li>
+          <NavLink to="/conta">
+            <Conta />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/conta/estatisticas">
+            <Estatisticas />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/conta/postar">
+            <Postar />
+          </NavLink>
+        </li>
+        <button onClick={logoutUser}><Sair/></button>
       </nav>
     </section>
   );
