f169bfe6d272d77c0db504382c7dbc48fcd8dd02
Gustavo Rodrigo Gularte Arend <gustavorrarend@gmail.com>
2023-12-12 15:59:59 -0300

    conta feed

--- src/Componentes/User/UserConta.jsx
+++ src/Componentes/User/UserConta.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import styles from './User.module.css';
 import Feed from '../Feed/Feed';
 import { UserContext } from '../../UserContext';
 
 const UserConta = () => {
   const { data } = React.useContext(UserContext);
 
-  return (
-    <section className={styles.conta + ' animeLeft'}>
-      <Feed user={data.id} />
-    </section>
-  );
+  return <Feed user={data.id} />;
 };
 
 export default UserConta;
